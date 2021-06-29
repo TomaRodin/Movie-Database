88ee1b98e56c43c71c60b2bcc90146028b438fde
Toma <70568187+TomaRodin@users.noreply.github.com>
2021-06-29 08:04:57 +0200

    int
    -Added users
    -Added watchlist

--- index.js
+++ index.js
@@ -76,6 +76,162 @@ app.get('/find/:number/:decade',function(req,res){
 
 
 
+
+
+app.get('/login',function(req,res){
+    if (req.cookies.LoggedIn == undefined) {
+        res.sendFile(__dirname+'/public/login.html')
+    } else {
+        res.redirect('/user')
+    }
+})
+
+
+app.post('/login',function(req,res){
+    const sqlite3 = require('sqlite3').verbose();
+    let db = new sqlite3.Database('movies.sqlite', sqlite3.OPEN_READWRITE, (err) => {
+
+    });
+    
+    
+    db.all("SELECT * FROM users WHERE name ="+"'"+req.body.name+"'"+"AND pass ="+"'"+req.body.pass+"'",function (err,rows){
+        var row = rows[0]
+        if (row == undefined) {
+            res.redirect('/login')
+        } else {
+            var row = rows[0]
+        res.cookie('user',row.id)
+        res.redirect('/user')
+        }
+    }) 
+        
+})
+
+
+app.get('/user',function(req,res){
+    if (req.cookies.user == undefined) {
+        res.redirect('/login')
+    } else {
+        const sqlite3 = require('sqlite3').verbose();
+        let db = new sqlite3.Database('movies.sqlite', sqlite3.OPEN_READWRITE, (err) => {
+    
+        });
+        db.all("SELECT * FROM users WHERE id ="+"'"+req.cookies.user+"'",function (err,rows){
+            var row = rows[0]
+            res.render(__dirname+'/public/user.ejs', {name:row.name,pass:row.pass})
+        })
+
+        
+    }
+})
+
+app.get('/user/log_out',function(req,res){
+    if (req.cookies.user == undefined) {
+        res.redirect('/')
+    }
+    else {
+        res.clearCookie('user');
+        res.redirect('/')
+    }
+})
+
+app.post('/save_watchlist',function(req,res){
+
+    if (req.cookies.user == undefined) {
+        res.redirect('/login')
+    }
+    else{
+            const sqlite3 = require('sqlite3').verbose();
+            let db = new sqlite3.Database('movies.sqlite', sqlite3.OPEN_READWRITE, (err) => {
+
+            });
+
+            db.all("SELECT * FROM watchlist WHERE movie ="+"'"+req.body.name+"'"+"AND id ="+"'"+req.cookies.user+"'",function (err,rows){
+                var row = rows[0]
+                if (row == undefined) {
+                    db.run("INSERT INTO watchlist (id,movie ) VALUES ('"+ req.cookies.user +"','"+req.body.name+"')");
+                }
+                else {
+                    return true
+                }
+            })    
+    }
+
+})
+
+
+app.get('/register',function(req,res){
+    if (req.cookies.user == undefined) {
+        res.sendFile(__dirname+'/public/register.html')
+    } else {
+        res.redirect('/user')
+    }
+})
+
+app.post('/register',function(req,res){
+    const sqlite3 = require('sqlite3').verbose();
+    let db = new sqlite3.Database('movies.sqlite', sqlite3.OPEN_READWRITE, (err) => {
+
+    });
+    db.all("SELECT * FROM users WHERE name ="+"'"+req.body.newname+"'",function (err,rows){
+        var row = rows[0]
+        if (row == undefined) {
+            db.all("SELECT * FROM users WHERE pass ="+"'"+req.body.newpass+"'",function (err,rows){
+                var row = rows[0]
+                if (row == undefined) {
+                    var uniqid = require('uniqid');
+                    var id = uniqid()
+                    db.run("INSERT INTO users (name,pass,id ) VALUES ('"+ req.body.newname +"','"+req.body.newpass+"','"+id+"')");
+                    res.redirect('/login')
+                } else {
+                    res.send('Exist')
+                }
+            })
+        } else {
+            res.send('Exist')
+        }
+    })
+})
+
+
+app.get('/watchlist',function(req,res){
+    const sqlite3 = require('sqlite3').verbose();
+    let db = new sqlite3.Database('movies.sqlite', sqlite3.OPEN_READWRITE, (err) => {
+
+    });
+    db.all("SELECT * FROM watchlist WHERE id ="+"'"+req.cookies.user+"'",function (err,rows){
+        console.log(rows)
+        res.json(rows)
+    })
+
+})
+
+app.get('/check_watchlist/:title',function(req,res){
+    const sqlite3 = require('sqlite3').verbose();
+    let db = new sqlite3.Database('movies.sqlite', sqlite3.OPEN_READWRITE, (err) => {
+
+    });
+
+    db.all("SELECT * FROM watchlist WHERE movie ="+"'"+req.params.title+"'"+"AND id ="+"'"+req.cookies.user+"'",function (err,rows){
+        res.json(rows)
+    })
+})
+
+
+app.post('/remove_watchlist',function(req,res){
+    if (req.cookies.user == undefined) {
+        res.redirect('/login')
+    }
+    else{
+            const sqlite3 = require('sqlite3').verbose();
+            let db = new sqlite3.Database('movies.sqlite', sqlite3.OPEN_READWRITE, (err) => {
+
+            });
+            db.run("DELETE FROM watchlist WHERE id ="+"'"+req.cookies.user+"'"+"AND movie ="+"'"+req.body.name+"'" );
+    }
+})
+
+
 app.get('/images/star.svg',function(req,res){
     res.sendFile(__dirname+'/public/image/star.svg')
 })
@@ -90,6 +246,10 @@ app.get('/images/director-chair.svg',function(req,res){
 
 app.get('/images/clapperboard.svg',function(req,res){
     res.sendFile(__dirname+'/public/image/clapperboard.svg')
+})
+
+app.get('/images/user.svg',function(req,res){
+    res.sendFile(__dirname+'/public/image/user.svg')
 })
 
 
